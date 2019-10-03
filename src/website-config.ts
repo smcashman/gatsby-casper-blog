7c1259552ffca00100d31616db0d3338787e0926
Sara Cashman <saramcashman@gmail.com>
2019-10-03 10:46:49 -0400

    Update website config with personal url

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,14 +49,14 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'The Web, Worldwide',
+  description: 'A blog about the future of the global web',
   coverImage: 'img/blog-cover.jpg',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
+  siteUrl: 'https://lucid-swirles-c53b3d.netlify.com',
   facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  twitter: 'https://twitter.com/smargaretc',
   showSubscribe: true,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
